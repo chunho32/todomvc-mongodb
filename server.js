a1432bfce23c211546c6ddc4872644f08ed22465
Hung Hoang <hung.information@gmail.com>
2018-03-23 16:23:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@
 
 	// configuration =================
 
-	mongoose.connect('mongodb://' + argv.be_ip + ':27017/my_database');
+	mongoose.connect('mongodb://' + 'localhost' + ':27017/my_database');
 
     	app.use('/js', express.static(__dirname + '/js'));
    	 app.use('/bower_components', express.static(__dirname + '/bower_components'));
@@ -123,5 +123,5 @@
 	});
 
 	// listen (start app with node server.js) ======================================
-	app.listen(8080, argv.fe_ip);
+	app.listen(8080);
 	console.log("App listening on port 8080");
